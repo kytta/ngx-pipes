264d3278508f2eb920c8800b2401efb954e3f5af
Nikita Karamov <me@kytta.dev>
2026-05-13 18:18:35 +0200

    fix: add alias for the typo'ed pipe

--- src/ng-pipes/pipes/array/truthify.ts
+++ src/ng-pipes/pipes/array/truthify.ts
@@ -6,6 +6,9 @@ export class TruthifyPipe implements PipeTransform {
   transform<T>(input: T): T;
 
   transform(input: any): any {
-    return Array.isArray(input) ? input.filter(e => !!e) : input;
+    return Array.isArray(input) ? input.filter((e) => !!e) : input;
   }
 }
+
+/** @deprecated this is a typo of {@link TruthifyPipe} and will be removed in v4 */
+export class TrurthifyPipe extends TruthifyPipe {};
