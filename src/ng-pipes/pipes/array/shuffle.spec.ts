79507773cc49693d1cb3cf312fe04ae55a710b1e
Nikita Karamov <me@kytta.dev>
2026-07-26 10:46:19 +0200

    test(shuffle): improve tests for ShufflePipe

--- src/ng-pipes/pipes/array/shuffle.spec.ts
+++ src/ng-pipes/pipes/array/shuffle.spec.ts
@@ -1,11 +1,7 @@
 import { ShufflePipe } from './shuffle';
 
 describe('ShufflePipe', () => {
-  let pipe: ShufflePipe;
-
-  beforeEach(() => {
-    pipe = new ShufflePipe();
-  });
+  const pipe = new ShufflePipe();
 
   it('should not change anything if not array', () => {
     expect(pipe.transform('foo')).toEqual('foo');
@@ -15,9 +11,28 @@ describe('ShufflePipe', () => {
     expect(pipe.transform({ foo: 1, bar: 2 })).toEqual({ foo: 1, bar: 2 });
   });
 
-  it('should return shuffled array', () => {
+  it('should keep empty arrays unchanged', () => {
+    expect(pipe.transform([])).toEqual([]);
+  });
+
+  it('should keep single-item arrays unchanged', () => {
     expect(pipe.transform([42])).toEqual([42]);
     expect(pipe.transform(['foo'])).toEqual(['foo']);
-    expect(pipe.transform([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).length).toEqual(10);
+  });
+
+  it('should return shuffled array', () => {
+    const original = [5, 1, 2, 5, 3, 2, 4, 4, 4];
+    const result = pipe.transform(original);
+
+    expect(result.length).toEqual(original.length);
+    expect([...result].sort()).toEqual([...original].sort());
+  });
+
+  it('should not mutate input', () => {
+    const original = [1, 2, 3, 4, 5];
+    const result = pipe.transform(original);
+
+    expect(result).not.toBe(original);
+    expect(original).toEqual([1, 2, 3, 4, 5]);
   });
 });
