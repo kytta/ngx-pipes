c5601767195a1230e75c51780e696786e8b50606
Nikita Karamov <me@kytta.dev>
2026-07-26 10:19:22 +0200

    test(test): improve tests for TestPipe

--- src/ng-pipes/pipes/string/test.spec.ts
+++ src/ng-pipes/pipes/string/test.spec.ts
@@ -1,20 +1,17 @@
 import { TestPipe } from './test';
 
 describe('TestPipe', () => {
-  let pipe: TestPipe;
-
-  beforeEach(() => {
-    pipe = new TestPipe();
-  });
+  const pipe = new TestPipe();
 
   it('should not do anything if not a string', () => {
     expect(pipe.transform(null, '')).toEqual(null);
     expect(pipe.transform(undefined, '')).toEqual(undefined);
     expect(pipe.transform(42, '')).toEqual(42);
+    expect(pipe.transform([42, 67], '')).toEqual([42, 67]);
     expect(pipe.transform({ name: 'foo' }, '')).toEqual({ name: 'foo' });
   });
 
-  it('should camelize properly', () => {
+  it('should test on strings', () => {
     expect(pipe.transform('foo 42', '[\\d]+$', 'g')).toBeTruthy();
     expect(pipe.transform('42 foo', '[\\d]+$', 'g')).toBeFalsy();
     expect(pipe.transform('foo', '[\\d]+$', 'g')).toBeFalsy();
