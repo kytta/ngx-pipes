27bc19309dead56357e3dce6297cf990d4d67f34
Nikita Karamov <me@kytta.dev>
2026-07-26 09:56:47 +0200

    test(floor): improve tests for FloorPipe (#38)

--- src/ng-pipes/pipes/math/floor.spec.ts
+++ src/ng-pipes/pipes/math/floor.spec.ts
@@ -1,18 +1,29 @@
 import { FloorPipe } from './floor';
 
 describe('FloorPipe', () => {
-  let pipe: FloorPipe;
+  const pipe = new FloorPipe();
 
-  beforeEach(() => {
-    pipe = new FloorPipe();
-  });
-
-  it('should return floor of given number', () => {
-    expect(pipe.transform(1.2345)).toEqual(1);
+  it('should return floor of given positive number', () => {
+    expect(pipe.transform(1)).toEqual(1);
     expect(pipe.transform(1.2345)).toEqual(1);
     expect(pipe.transform(42.123)).toEqual(42);
+  });
+
+  it('should return floor of given negative number', () => {
+    expect(pipe.transform(-1)).toEqual(-1);
+    expect(pipe.transform(-1.2345)).toEqual(-2);
+    expect(pipe.transform(-42.123)).toEqual(-43);
+  });
+
+  it('should return floor with given precision', () => {
     expect(pipe.transform(42.123, 1)).toEqual(42.1);
     expect(pipe.transform(42.4242, 2)).toEqual(42.42);
+  });
+
+  it('should ignore negative precision', () => {
     expect(pipe.transform(42, -1)).toEqual(42);
+    expect(pipe.transform(-42, -1)).toEqual(-42);
+    expect(pipe.transform(1.2345, -1)).toEqual(1);
+    expect(pipe.transform(-1.2345, -1)).toEqual(-2);
   });
 });
