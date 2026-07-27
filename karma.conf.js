67a3b1f25e8eb79281a906ab4a64e468f3394a9c
Nikita Karamov <me@kytta.dev>
2026-07-27 16:17:42 +0200

    test: fix local test runs errorring (#49)

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: config.singleRun // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
       suppressAll: true // removes the duplicated traces
