e6beddcbb8f5b39f23052cab7e3ff73bc5459853
Nikita Karamov <me@kytta.dev>
2026-07-27 20:28:21 +0200

    test(trim): improve tests for TrimPipes (#57)

--- src/ng-pipes/pipes/string/ltrim.spec.ts
+++ src/ng-pipes/pipes/string/ltrim.spec.ts
@@ -1,19 +1,36 @@
 import { LeftTrimPipe } from './ltrim';
 
 describe('LeftTrimPipe', () => {
-  let pipe: LeftTrimPipe;
-
-  beforeEach(() => {
-    pipe = new LeftTrimPipe();
-  });
+  const pipe = new LeftTrimPipe();
 
   it('should left trim whitespace from string', () => {
-    const result = pipe.transform('   foo bar   ');
-    expect(result).toEqual('foo bar   ');
+    expect(pipe.transform('   foo bar   ')).toEqual('foo bar   ');
   });
 
   it('should left trim other characters from string', () => {
-    const result = pipe.transform('42foo bar42', '42');
-    expect(result).toEqual('foo bar42');
+    expect(pipe.transform('42foo bar42', '42')).toEqual('foo bar42');
+  });
+
+  it('should not modify already trimmed string', () => {
+    expect(pipe.transform('foo bar  ')).toEqual('foo bar  ');
+  });
+
+  it('should not modify falsy strings', () => {
+    expect(pipe.transform('')).toEqual('');
+    // @ts-ignore
+    expect(pipe.transform(undefined)).toEqual(undefined);
+    // @ts-ignore
+    expect(pipe.transform(null)).toEqual(null);
+  });
+
+  it('should not modify input on empty trimchars', () => {
+    expect(pipe.transform(' foo ', '')).toEqual(' foo ');
+  });
+
+  it('should not mutate string', () => {
+    const original = '   foo bar   ';
+
+    expect(pipe.transform(original)).not.toBe(original);
+    expect(original).toBe('   foo bar   ');
   });
 });
--- src/ng-pipes/pipes/string/trim.spec.ts
+++ src/ng-pipes/pipes/string/trim.spec.ts
@@ -1,19 +1,36 @@
 import { TrimPipe } from './trim';
 
 describe('TrimPipe', () => {
-  let pipe: TrimPipe;
-
-  beforeEach(() => {
-    pipe = new TrimPipe();
-  });
+  const pipe = new TrimPipe();
 
   it('should trim whitespace from string', () => {
-    const result = pipe.transform('   foo bar   ');
-    expect(result).toEqual('foo bar');
+    expect(pipe.transform('   foo bar   ')).toEqual('foo bar');
   });
 
   it('should trim other characters from string', () => {
-    const result = pipe.transform('42foo bar4242', '42');
-    expect(result).toEqual('foo bar');
+    expect(pipe.transform('42foo bar4242', '42')).toEqual('foo bar');
+  });
+
+  it('should not modify already trimmed string', () => {
+    expect(pipe.transform('foo bar')).toEqual('foo bar');
+  });
+
+  it('should not modify falsy strings', () => {
+    expect(pipe.transform('')).toEqual('');
+    // @ts-ignore
+    expect(pipe.transform(undefined)).toEqual(undefined);
+    // @ts-ignore
+    expect(pipe.transform(null)).toEqual(null);
+  });
+
+  it('should not modify input on empty trimchars', () => {
+    expect(pipe.transform(' foo ', '')).toEqual(' foo ');
+  });
+
+  it('should not mutate string', () => {
+    const original = '   foo bar   ';
+
+    expect(pipe.transform(original)).not.toBe(original);
+    expect(original).toBe('   foo bar   ');
   });
 });
